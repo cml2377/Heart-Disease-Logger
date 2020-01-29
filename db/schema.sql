10530d89869db8895535284225d2a1f94f87234a
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-29 17:49:44 -0600

    added database info for burgers

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,18 @@
 -- Burger Database -- 
 
-Create the
-`burgers_db`.
-   * Switch to or
-use the
-`burgers_db`.
-   *
-Create a `burgers` table
-with these
-fields:
-* **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+CREATE TABLE burgers
+(
+   id INT
+   AUTO_INCREMENT PRIMARY KEY,
+   burgerName VARCHAR
+   (200),
+   devoured BOOLEAN
+);
+
